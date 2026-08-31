e93240f59930a976d745437e399698f976f28417
Jenny Cao <jennycamcao@gmail.com>
2026-08-30 19:40:30 -0700

    Add button

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function FirstImage(){
     let myImage = document.querySelector('#myImage')
     myImage.src = "attachments/airplane2.jpg"
 }
+
+document.getElementById("backHome").onclick = function (){
+    location.href = "index.html";
+};
